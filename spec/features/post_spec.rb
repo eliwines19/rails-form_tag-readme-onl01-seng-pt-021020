6e9e2a4f52dd06c76d8c382e1c3f0f5cf999a5dd
Elijah wines <elijahwines84@yahoo.com>
2020-06-15 14:51:52 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'new_post' do
   it 'ensures that the form route works with the /new action' do
-    visits new_post_path
+    visit new_post_path
     expect(page.status_code).to eq(200)
   end
 end
